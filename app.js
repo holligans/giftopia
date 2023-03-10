a0fc6dd08a14c29383c6e7066ad4924a27bb6e40
holligans1985 <alexfranc1985@gmail.com>
2023-03-10 16:56:49 -0500

    creates server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const a = "Alex";
+const http =  require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!'
+  }));
+});
+
+server.listen(5000);
